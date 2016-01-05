af8984755b5fed4e0380a9183ef553c9aa01f53f
CJ Patoilo <cjpatoilo@gmail.com>
2016-01-05 00:21:49 -0300

    Improvements in meta tag

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>µg</title>
-  <link rel="stylesheet" href="css/normalize.css">
+  <meta charset="utf-8">
+  <meta name="robots" content="index, follow">
+  <meta name="theme-color" content="#f4f5f6">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#f4f5f6">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="https://ug-web.herokuapp.com/">
+  <meta name="description" content="Milligram framework customizer">
+  <meta property="og:description" content="Milligram framework customizer">
+  <meta property="og:image" content="https://ug-web.herokuapp.com/img/thumbnail.jpg">
+  <meta property="og:locale" content="en">
+  <meta property="og:site_name" content="µg | A Milligram customizer">
+  <meta property="og:title" content="µg | A Milligram customizer">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://ug-web.herokuapp.com/">
+  <meta property="article:published_time" content="2015-12-25T01:00:00Z">
+  <meta property="article:author" content="@vkeusebio">
+  <meta property="article:section" content="website">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:domain" content="µg | A Milligram customizer">
+  <meta name="twitter:url" content="https://ug-web.herokuapp.com/">
+  <meta name="twitter:site" content="https://ug-web.herokuapp.com/">
+  <meta name="twitter:creator" content="@vkeusebio">
+  <meta name="twitter:title" content="µg | A Milligram customizer">
+  <meta name="twitter:description" content="Milligram framework customizer">
+  <meta name="twitter:image:src" content="https://ug-web.herokuapp.com/img/thumbnail.jpg">
+  <title>µg | A Milligram customizer</title>
+  <link rel="canonical" href="https://ug-web.herokuapp.com/">
+  <link rel="image_src" href="https://ug-web.herokuapp.com/img/thumbnail.jpg">
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
   <link rel="stylesheet" href="css/milligram.min.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
   <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="icon" href="img/favicon.png">
 </head>
 <body>
   <div class="wrapper">
